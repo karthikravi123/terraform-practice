cf4cd53ef6926c76b936befa813b196b39ba8013
karthik <86100724+karthikravi123@users.noreply.github.com>
2024-01-13 21:05:07 +0530

    Add files via upload

--- 05-section5/c5-ec2instance.tf
+++ 05-section5/c5-ec2instance.tf
@@ -1,14 +1,17 @@
 #resource: ec2 instance
 resource "aws_instance" "myec2" {
   ami = data.aws_ami.amzlinux2.id
-  instance_type = var.instance_type
+  #instance_type = var.instance_type
+  instance_type =  var.instance_type_list[1]  #list
+  #instance_type =  var.instance_type_map["prod"]  #map
   user_data = file("${path.module}/install.sh")
   key_name = var.instance_keypair
   vpc_security_group_ids = [ 
     aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id
    ]
+  count = 2
   tags = {
-   "Name" = "ec2-demo"
+   "Name" = "count-demo-${count.index}"
   } 
  
 }
--- 05-section5/c6-output.tf
+++ 05-section5/c6-output.tf
@@ -14,3 +14,40 @@ output "instance_public_dns" {
     value = aws_instance.myec2.public_dns
   
 }
+
+
+#output - for loops with list
+output "for output_list" {
+    description = "for loop with list"
+    value = [for instance in aws_instance.myec2: instance.publicdns]
+  
+}
+
+#output - for loop with map
+output "for output_map" {
+    description = "for loop with map"
+    value = {for instance in aws_instance.myec2: instance.id => instance.publicdns}
+  
+}
+
+#output -for loops with map advance
+output "for output_map2" {
+    description = "for loop with map advance"
+    value = {for c, instance in aws_aws_instance.myec2: c=> instance.public_dns}
+  
+}
+
+#output legacy splat operator (legacy) - return the list
+output "legacy_splat_instance_publicdns" {
+    description = "legacy splat operator"
+    value = aws_instance.myec2.*.public_dns
+  
+}
+
+
+#output latest generalized splat operator - return the list
+output "latest_splat_instance_publicdns" {
+    description = "genearalized latest splat operator"
+    value = aws_instance.myec2[*].public_dns
+  
+}
